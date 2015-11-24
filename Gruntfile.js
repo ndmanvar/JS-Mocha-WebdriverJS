a3a98734e2268c60d433a6492e928070520ca4d4
Kevin Berg <kevinberg@Kevins-MacBook-Pro.local>
2015-11-24 15:29:14 -0800

    Add more browsers

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function (grunt) {
             options: {
                 grunt: true
             },
-            tasks: ['run_XP_firefox_42', 'run_Linux_chrome_45']
+            tasks: ['run_XP_firefox_42', 'run_Linux_chrome_45', 'run_Windows10_edge', 'run_Windows7_ie_11']
         }
       }
     });
@@ -30,4 +30,6 @@ module.exports = function (grunt) {
 
     grunt.registerTask('run_XP_firefox_42', ['shell:runTests:XP:firefox:42']);
     grunt.registerTask('run_Linux_chrome_45', ['shell:runTests:Linux:chrome:45']);
+    grunt.registerTask('run_Windows10_edge', ['shell:runTests:"Windows 10":MicrosoftEdge:20.10240']);
+    grunt.registerTask('run_Windows7_ie_11', ['shell:runTests:"Windows 7":"internet explorer":11']);
 };
